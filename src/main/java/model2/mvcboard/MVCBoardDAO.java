36926df4bd2094a39936887f01eee92f17dbbcee
Kwon <kwonabraham@naver.com>
2024-03-25 21:36:32 +0900

    데이터 DB추가 DAO 작성

--- src/main/java/model2/mvcboard/MVCBoardDAO.java
+++ src/main/java/model2/mvcboard/MVCBoardDAO.java
@@ -82,4 +82,26 @@ public class MVCBoardDAO extends DBConnPool {
         return board;
     }
 
+    public int insertWrite(MVCBoardDTO dto) {
+        int result = 0;
+        try {
+            String query = "INSERT INTO mvcboard ( "
+                         + " idx, name, title, content, ofile, sfile, pass) "
+                         + " VALUES ( "
+                         + " seq_board_num.NEXTVAL,?,?,?,?,?,?)";
+            psmt = con.prepareStatement(query);
+            psmt.setString(1, dto.getName());
+            psmt.setString(2, dto.getTitle());
+            psmt.setString(3, dto.getContent());
+            psmt.setString(4, dto.getOfile());
+            psmt.setString(5, dto.getSfile());
+            psmt.setString(6, dto.getPass());
+            result = psmt.executeUpdate();
+        }
+        catch (Exception e) {
+            System.out.println("게시물 입력 중 예외 발생");
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
