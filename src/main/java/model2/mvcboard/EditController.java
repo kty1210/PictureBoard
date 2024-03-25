18fb795fe9efb92bf3c32f1aad13285a837c74a4
Kwon <kwonabraham@naver.com>
2024-03-25 23:54:53 +0900

    수정 컨트롤러 생성

---
+++ src/main/java/model2/mvcboard/EditController.java
@@ -0,0 +1,31 @@
+package model2.mvcboard;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mvcboard/edit.do")
+@MultipartConfig(
+	maxFileSize = 1024 * 1024 * 1,
+	maxRequestSize = 1024 * 1024 * 10
+)
+public class EditController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String idx = req.getParameter("idx");
+        MVCBoardDAO dao = new MVCBoardDAO();
+        MVCBoardDTO dto = dao.selectView(idx);
+        req.setAttribute("dto", dto);
+        req.getRequestDispatcher("/14MVCBoard/Edit.jsp").forward(req, resp);
+    }
+
+    
+}
