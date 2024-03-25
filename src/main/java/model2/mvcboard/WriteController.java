01aacdfbf8b631f51a13989aae4ca0f1ebecf4b6
Kwon <kwonabraham@naver.com>
2024-03-25 21:33:28 +0900

    writeController 완료 & write jsp 페이지 생성

--- src/main/java/model2/mvcboard/WriteController.java
+++ src/main/java/model2/mvcboard/WriteController.java
@@ -1,19 +1,17 @@
 package model2.mvcboard;
 
 import java.io.IOException;
-import java.net.http.HttpResponse;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse; // HttpResponse 대신 HttpServletResponse 사용
 
-
-public class WriteController extends HttpServlet{
-	@Override
-	protected void doGet(HttpServletRequest req, HttpResponse resp)
-		throws ServletException, IOException{
-		req.getRequestDispatcher("/14MVCBoard/Write.jsp").forward(req, resp);
-		
-	}
+public class WriteController extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        req.getRequestDispatcher("/14MVCBoard/Write.jsp").forward(req, resp);
+    }
 }
-
